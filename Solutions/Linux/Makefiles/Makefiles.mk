92fc6cce7cc5404fcea606442bde517dc2889e82
Guillaume Riflet <guillaume.riflet@gmail.com>
2009-07-17 14:29:01 +0000

    added missing file in Linux solution
    git-svn-id: https://mohid.svn.codeplex.com/svn@29361 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ Solutions/Linux/Makefiles/Makefiles.mk
@@ -0,0 +1,33 @@
+OBJS = $(FILES:.$(S)=.$(O))
+SRCF = $(SRCDIR)
+SUFF := $(suffix $(TARGET))
+
+#Make all
+.PHONY: all
+all: $(TARGET)
+
+$(TARGET) : $(OBJS) $(LIBS)
+ifeq ($(SUFF),$(SUFFLIB))
+	@$(AR) $@ $^
+else
+	@$(CC) $(LFLAGS) -o $@ $^ $(LLFLAGS)
+endif
+	@echo Finished building $@.
+
+#Fortran compilation rule
+%.$(O) : $(SRCF)/%.$(F)
+	@$(CC) $(CCFLAGS) $(INCS) $<
+	@echo $* .................. [OK]
+
+#make clean
+.PHONY: clean
+clean:
+	@-$(DEL) *.$(O) *.$(MOD) $(TARGET)
+	@echo erased $(TARGET) files.
+
+#make install
+SOURCE := $(TARGET)
+.PHONY: install
+install: $(SOURCE)
+	@-$(CP) $< $(DESTDIR)/`date +%G%m%d`_$(addprefix $(VER), $<)
+	@echo Installed $<.
