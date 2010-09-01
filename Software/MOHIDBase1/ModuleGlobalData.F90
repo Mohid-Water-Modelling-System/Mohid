c61eec46e5fac23b1f717961ce136dbb849f1c4b
Paulo Chambel <paulo.chambel@hidromod.com>
2010-09-01 13:16:16 +0000

    A new compute cell point was added (W - bottom/top faces o Z control volume). A new parameter related with a new way of computing the baroclinic force.
    git-svn-id: https://mohid.svn.codeplex.com/svn@56931 81fbe566-5dc4-48c1-bdea-7421811ca204

--- Software/MOHIDBase1/ModuleGlobalData.F90
+++ Software/MOHIDBase1/ModuleGlobalData.F90
@@ -78,6 +78,9 @@ Module ModuleGlobalData
     integer, parameter  :: MaxInstances         = 500
 #endif
 
+    integer, parameter  :: MaxErrorMessages     = 20
+    integer             :: NumberOfErrorMessages=0
+
     integer, parameter  :: StringLength         = 128
     integer, parameter  :: PathLength           = 256
 
@@ -195,6 +198,9 @@ Module ModuleGlobalData
     integer, parameter :: Rectang_         = 3
 
 
+    !Mohid Land ground water flow type
+    integer, parameter :: GWFlowToChanByCell_               = 1
+    integer, parameter :: GWFlowToChanByLayer_              = 2
 
     !Water Properties 
     integer, parameter :: Density_                          =  0        
@@ -438,6 +444,7 @@ Module ModuleGlobalData
     integer, parameter :: SpecificLeafStorage_              = 715
     integer, parameter :: EVTPCropCoefficient_              = 716
     integer, parameter :: CanopyHeight_                     = 718
+    integer, parameter :: PotLeafAreaIndex_                 = 719
 
 !____used @ moduleCEQUALW2______________________________________________
     integer, parameter :: RPOM_                             = 2001
@@ -652,6 +659,10 @@ Module ModuleGlobalData
     integer, parameter :: SolutionNitrite_                 = 10007
     integer, parameter :: SolutionChlorine_                = 10008
     integer, parameter :: SolutionCarbon_                  = 10009
+    integer, parameter :: SolutionPotassium_               = 10010
+    integer, parameter :: SolutionAluminium_               = 10011
+    integer, parameter :: SolutionSilicium_                = 10012
+    
     
     !rain concentrantion properties
     integer, parameter :: RainMagnesium_                   = 10201
@@ -665,6 +676,7 @@ Module ModuleGlobalData
     integer, parameter :: eMgX2_                           = 10302
     integer, parameter :: eNaX_                            = 10303
     integer, parameter :: eNH4X_                           = 10304
+    integer, parameter :: eKX_                             = 10305
     
     !Species properties
     integer, parameter :: sCa2_                            = 10501
@@ -696,6 +708,7 @@ Module ModuleGlobalData
     integer, parameter :: Dolomite_                        = 11501 !CaMg(CO3)2
     integer, parameter :: Aragonite_                       = 11502 !CaCO3
     integer, parameter :: Halite_                          = 11503 !NaCl
+    integer, parameter :: KFeldspar_                       = 11504 !KAlSi3O8
 
     !Other PhreeqC properties
     integer, parameter :: pE_                              = 12000
@@ -739,6 +752,7 @@ Module ModuleGlobalData
     character(StringLength), private, parameter :: Char_eCaX2                = 'CaX2'
     character(StringLength), private, parameter :: Char_eMgX2                = 'MgX2'
     character(StringLength), private, parameter :: Char_eNaX                 = 'NaX'
+    character(StringLength), private, parameter :: Char_eKX                  = 'KX'
     character(StringLength), private, parameter :: Char_eNH4X                = 'AmmoniaX' !'NH4X'
     character(StringLength), private, parameter :: Char_sCa2                 = 'Ca+2'
     character(StringLength), private, parameter :: Char_sCaOH                = 'CaOH+'
@@ -761,12 +775,17 @@ Module ModuleGlobalData
     character(StringLength), private, parameter :: Char_Dolomite             = 'dolomite'
     character(StringLength), private, parameter :: Char_Aragonite            = 'aragonite'
     character(StringLength), private, parameter :: Char_Halite               = 'halite' 
-    character(StringLength), private, parameter :: char_SolutionCarbon       = 'solution carbon' 
-    character(StringLength), private, parameter :: char_RainMagnesium        = 'rain magnesium' 
-    character(StringLength), private, parameter :: char_RainCalcium          = 'rain calcium' 
-    character(StringLength), private, parameter :: char_RainSodium           = 'rain sodium' 
-    character(StringLength), private, parameter :: char_RainChlorine         = 'rain chlorine'
-    character(StringLength), private, parameter :: char_RainAmmonia          = 'rain ammonia'
+    character(StringLength), private, parameter :: Char_KFeldspar            = 'k-feldspar'
+    character(StringLength), private, parameter :: Char_SolutionCarbon       = 'solution carbon' 
+    character(StringLength), private, parameter :: Char_SolutionPotassium    = 'solution potassium'     
+    character(StringLength), private, parameter :: Char_SolutionAluminium    = 'solution aluminium' 
+    character(StringLength), private, parameter :: Char_SolutionSilicium     = 'solution silicium' 
+    character(StringLength), private, parameter :: Char_RainMagnesium        = 'rain magnesium' 
+    character(StringLength), private, parameter :: Char_RainCalcium          = 'rain calcium' 
+    character(StringLength), private, parameter :: Char_RainSodium           = 'rain sodium' 
+    character(StringLength), private, parameter :: Char_RainChlorine         = 'rain chlorine'
+    character(StringLength), private, parameter :: Char_RainAmmonia          = 'rain ammonia'
+
 
 
 !_______________________________________________________________________________________________
@@ -1203,6 +1222,7 @@ Module ModuleGlobalData
     character(StringLength), private, parameter :: Char_SpecificLeafStorage      = 'specific leaf storage'
     character(StringLength), private, parameter :: Char_EVTPCropCoefficient      = 'crop coefficient'
     character(StringLength), private, parameter :: Char_CanopyHeight             = 'canopy height'
+    character(StringLength), private, parameter :: Char_PotLeafAreaIndex         = 'potential leaf area index'
 
     !Cohesive Fractions - Drainage Network
     character(StringLength), private, parameter :: Char_TSS                      = 'TSS'
@@ -1502,7 +1522,7 @@ Module ModuleGlobalData
           T_Module(mBASIN_                  , "Basin"),                 T_Module(mSOILPROPERTIES_         , "SoilProperties"),        &
           T_Module(mINFILTRATION_           , "Infiltration"),          T_Module(mSOILPLANTAIR_           , "SoilPlantAir"),          &
           T_Module(mSOILMACROPORES_         , "SoilMacropores"),        T_Module(mSAND_                   , "Sand"),                  &
-          T_Module(mMACROPOREPROPERTIES_    , "MacroporeProperties"),   T_Module(mPOROUSMEDIA_           , "PorousMedia"),           &
+          T_Module(mMACROPOREPROPERTIES_    , "MacroporeProperties"),   T_Module(mPOROUSMEDIA_            , "PorousMedia"),           &
           T_Module(mSWAT_                   , "ModuleSwat"),            T_Module(mPROFILE_                , "Profile"),               &
           T_Module(mBENTHOS_                , "Benthos"),               T_Module(mCLIMATOLOGY_            , "Climatology"),           &
           T_Module(mFIREINDEX_              , "FireIndex"),             T_Module(mINTERPOLATION_          , "Interpolation"),         &
@@ -1523,7 +1543,9 @@ Module ModuleGlobalData
     integer                                                         :: PropertiesNumber
     integer, private                                                :: ErrorFileID     = 0
     integer, private                                                :: UsedKeyFileID   = 0
+    integer, private                                                :: LogFileID       = 0
     character(LEN=1024)                                             :: OnLineString
+    character(StringLength),     dimension(MaxErrorMessages)        :: ErrorMessagesStack
       
     type (T_Instance), dimension (MaxModules, MaxInstances), save   :: ObjCollector
     private :: ObjCollector
@@ -2206,6 +2228,7 @@ Module ModuleGlobalData
             call AddPropList (SpecificLeafStorage_,     Char_SpecificLeafStorage,        ListNumber)
             call AddPropList (EVTPCropCoefficient_,     Char_EVTPCropCoefficient,        ListNumber)
             call AddPropList (CanopyHeight_,            Char_CanopyHeight,               ListNumber)
+            call AddPropList (PotLeafAreaIndex_,        Char_PotLeafAreaIndex,           ListNumber)
 
             call AddPropList (TSS_,                     Char_TSS,                        ListNumber)
             call AddPropList (COHSED_FINE_,             Char_Cohsed_fine,                ListNumber)
@@ -2230,6 +2253,7 @@ Module ModuleGlobalData
             call AddPropList (eMgX2_,                   Char_eMgX2,                      ListNumber)
             call AddPropList (eNaX_,                    Char_eNaX,                       ListNumber)
             call AddPropList (eNH4X_,                   Char_eNH4X,                      ListNumber)
+            call AddPropList (eKX_,                     Char_eKX,                        ListNumber)
             call AddPropList (sCa2_,                    Char_sCa2,                       ListNumber)
             call AddPropList (sCaOH_,                   Char_sCaOH,                      ListNumber)
             call AddPropList (sH2_,                     Char_sH2,                        ListNumber)
@@ -2251,7 +2275,11 @@ Module ModuleGlobalData
             call AddPropList (Dolomite_,                Char_Dolomite,                   ListNumber)
             call AddPropList (Aragonite_,               Char_Aragonite,                  ListNumber)
             call AddPropList (Halite_,                  Char_Halite,                     ListNumber)
+            call AddPropList (KFeldspar_,               Char_KFeldspar,                  ListNumber)
             call AddPropList (SolutionCarbon_,          Char_SolutionCarbon,             ListNumber) 
+            call AddPropList (SolutionPotassium_,       Char_SolutionPotassium,          ListNumber) 
+            call AddPropList (SolutionAluminium_,       Char_SolutionAluminium,          ListNumber) 
+            call AddPropList (SolutionSilicium_,        Char_SolutionSilicium,           ListNumber) 
             call AddPropList (RainMagnesium_,           Char_RainMagnesium,              ListNumber) 
             call AddPropList (RainCalcium_,             Char_RainCalcium,                ListNumber) 
             call AddPropList (RainSodium_,              Char_RainSodium,                 ListNumber)
@@ -2614,6 +2642,9 @@ cd7 :   if (Screen_) then
             write(*,                *     ) 
         end if cd7
 
+        !Places Message on the Message Stack
+        call PlaceErrorMessageOnStack(trim(adjustl(SmallMessage)))
+
         !If the error message isn"t a warning, stop the execution of this process
 cd3 :   if (ErrorMagnitude == FATAL_) then
             close (unit=ErrorFileID  )
@@ -2673,6 +2704,9 @@ cd2 :   if      (ErrorType == INTERNAL_  ) then
                                 int(Year), int(Month),  int(Day),               &
                                 int(Hour), int(Minute), int(Second)
   
+        !Places Message on the Message Stack
+        call PlaceErrorMessageOnStack(trim(adjustl(SmallMessage)))
+
 cd7 :   if (Screen_) then
             write(*,*) 
             write(*,555) trim(adjustl(StrErrorMagnitude))//semicolumn//     &
@@ -2694,6 +2728,38 @@ cd3 :   if (ErrorMagnitude == FATAL_) then
     end subroutine SetErrorTime
 
     !----------------------------------------------------------------------
+    
+    !Messages are stored in a stack: Last written = Highest number.
+    !First written is discarded if the stack size is exceeded.
+    subroutine PlaceErrorMessageOnStack(ErrorMessage)
+    
+        !Arguments-------------------------------------------------------------
+        character(len=*)                            :: ErrorMessage
+        
+        !Local-----------------------------------------------------------------
+        integer                                     :: i
+
+        if (NumberOfErrorMessages == 0) then
+            do i=1, MaxErrorMessages
+                ErrorMessagesStack(i)=' '
+            enddo
+        endif
+
+        NumberOfErrorMessages =NumberOfErrorMessages + 1
+
+        if(NumberOfErrorMessages.gt.MaxErrorMessages)then
+            NumberOfErrorMessages=MaxErrorMessages
+            do i=1, NumberOfErrorMessages-1
+                ErrorMessagesStack(i)=ErrorMessagesStack(i+1)
+            enddo
+        endif
+
+        ErrorMessagesStack(NumberOfErrorMessages)=ErrorMessage
+       
+    end subroutine PlaceErrorMessageOnStack
+
+    !----------------------------------------------------------------------
+
 
     subroutine WriteDTLog (ModuleName, iter, DT)
 
@@ -2735,6 +2801,7 @@ cd3 :   if (ErrorMagnitude == FATAL_) then
         Seed = 1
         call RANDOM_SEED(PUT  = Seed)
 
+#ifndef _OPENMI_
         write(*, *)"-------------------------- MOHID -------------------------"
         write(*, *)
         write(*, *)"      AUTHOR   : IST/MARETEC, Marine Modelling Group      "
@@ -2747,6 +2814,7 @@ cd3 :   if (ErrorMagnitude == FATAL_) then
         write(*, *)
         write(*, *)"Constructing "//ModelName
         write(*, *)"Please Wait..."
+#endif
 
 #ifndef _ONLINE_ 
 
@@ -2831,6 +2899,7 @@ do2:    do
         ElapsedHours = INT(Elapsedseconds/3600)
         ElapsedMinutes = INT((ElapsedSeconds-ElapsedHours*3600)/60)
         ElapsedSecremain = INT((ElapsedSeconds-ElapsedMinutes*60-ElapsedHours*3600))
+#ifndef _OPENMI_
         write(*, *)"-------------------------- MOHID -------------------------"
         write(*, *)
         write(*, *)"Program "//ModelName//" successfully terminated"
@@ -2848,7 +2917,10 @@ do2:    do
         endif
         write(*, *)
         write(*, *)"----------------------------------------------------------"
+        
+        !This Stop Statement has been removed because it makes OpenMI Destructor work improberly
         stop
+#endif
 
     110 format(1x, "Total Elapsed Time     : ",f14.2," ",i3,"h ",i2,"min ",i2,"s",/)
     120 format(1x, "Total CPU time         : ",f14.2,/)
