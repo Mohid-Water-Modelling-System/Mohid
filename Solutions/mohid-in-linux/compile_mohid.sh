9642b192bc163530149ae12ff5c03a8201acd4cb
Jorge Palma <xjpalma@gmail.com>
2018-09-20 15:55:31 +0100

    update compile flags to mpi mode

--- Solutions/mohid-in-linux/compile_mohid.sh
+++ Solutions/mohid-in-linux/compile_mohid.sh
@@ -23,8 +23,8 @@ DIR_REQ=$HOME/apps_intel
 ZLIB=$DIR_REQ/zlib-1.2.11
 HDF5=$DIR_REQ/hdf5-1.8.15
 NETCDF=$DIR_REQ/netcdf-4.4.1.1
-#MPI=$DIR_REQ/mpich-3.2
-MPI=/opt/intel/compilers_and_libraries/linux/mpi/intel64
+MPI=$DIR_REQ/mpich-3.2
+#MPI=/opt/intel/compilers_and_libraries/linux/mpi/intel64
 PROJ4=$DIR_REQ/proj-4.9.3
 PROJ4F=$DIR_REQ/proj4-fortran
 IPHREEQC=$DIR_REQ/iphreeqc-3.3.11-12535
@@ -213,8 +213,8 @@ elif [[ $FC == *"ifort"* ]]; then
     
     ## -fp-model source: Control the tradeoffs between accuracy, reproducibility and performance and
     ## improve the consistency and reproducibility of floating-point results while limiting the impact on performance.
-    OPT_FLAGS="-O3 -convert little_endian -fPIC -heap-arrays 64 -fp-model source " #-mcmodel=large
-       
+    OPT_FLAGS="-O1 -convert little_endian -fPIC -heap-arrays 64 -fp-model source" #-mcmodel=large
+  
     OTH_FLAGS=" -xHost -ip -fpe0  -fpp "
     
     MODOUT="-module "
@@ -686,13 +686,13 @@ MOHID_TOOLS(){
     ConvertGridDataToHDF5 \
     ConvertHDF5ToGridData \
     ConvertToHDF5 \
-    ConvertToXYZ
+    ConvertToXYZ \
     DigitalTerrainCreator \
     FillMatrix \
     HDF5Exporter \
     HDF5Extractor \
     HDF5Statistics \
-    DomainDecompositionConsolidation \
+    MainDDC \
     #Shell \   ## error
     )
 
@@ -722,6 +722,7 @@ MOHID_TOOLS(){
         ModuleHYCOMFormat \
         ModuleIHRadarFormat
         ModuleMERCATORFormat \
+        ModuleDelft3D_2_Mohid \
         ModuleMOG2DFormat \
         ModuleReadSWANNonStationary \
         ModuleWOAFormat \
@@ -749,13 +750,14 @@ MOHID_TOOLS(){
         ModuleEtopo5 \
         ModuleGEBCO \
         ModuleNASA \
-        ModuleNOAA_ShoreLine \
+        ModuleNOAA_ShoreLine \ 
         ModuleSRTM30)
         COMPILE_MOHID_TOOLS modules_ConvertToXYZ "$tool"
 
     elif [ $tool = 'HDF5Exporter' ]; then
       modules_HDF5Exporter=( \
-        ModuleExportHDF5ToTimeSerie)
+        ModuleExportHDF5ToTimeSerie \
+        )
         COMPILE_MOHID_TOOLS modules_HDF5Exporter "$tool"
 
     elif [ $tool = 'HDF5Extractor' ]; then
@@ -768,7 +770,7 @@ MOHID_TOOLS(){
         ModuleHDF5Statistics)
         COMPILE_MOHID_TOOLS modules_HDF5Statistics "$tool"
 
-    elif [ $tool = 'DomainDecompositionConsolidation' ]; then
+    elif [ $tool = 'MainDDC' ]; then
       modules_DDC=( \
         ModuleHashTable \
         ModuleDDC)
