afaf43569d923100e28aa9a861654f16dda1f62d
JoaoBSobrinho <joao.sobrinho@tecnico.ulisboa.pt>
2019-01-09 20:01:28 +0000

    Moved older configurations
    Moded oldest 2 configurations to folder Older_Configurations

---
+++ Solutions/Older_Configurations/VisualStudio2008_IntelFortran11/MOHIDNumerics/MOHID.OpenMI.MohidLand.Wrapper/MohidLandLinkableComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MOHID.OpenMI.MohidLand.Wrapper
+{
+    public class MohidLandLinkableComponent : Oatc.OpenMI.Sdk.Wrapper.LinkableEngine
+    {
+        public MohidLandLinkableComponent()
+        {
+            _engineApiAccess = new MohidLandEngineWrapper();
+        }
+
+        protected override void SetEngineApiAccess()
+        {
+            _engineApiAccess = new MohidLandEngineWrapper();
+        }
+
+
+    }
+}
